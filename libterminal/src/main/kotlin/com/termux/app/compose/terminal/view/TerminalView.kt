7195328ac10df4fbb43c3822eb6b9ec487ac5b8b
极犽 <kr.nickel@outlook.com>
2026-09-21 18:37:25 +0800

    bugfix: 修复nova3.1.1 无法获得触摸焦点导致无法获得命令输入的问题

--- libterminal/src/main/kotlin/com/termux/app/compose/terminal/view/TerminalView.kt
+++ libterminal/src/main/kotlin/com/termux/app/compose/terminal/view/TerminalView.kt
@@ -545,6 +545,9 @@ class TerminalView(
 
     init {
         isVerticalScrollBarEnabled = true
+        isFocusable = true
+        isFocusableInTouchMode = true
+        defaultFocusHighlightEnabled = false
     }
 
     private val Int.dp: Int
