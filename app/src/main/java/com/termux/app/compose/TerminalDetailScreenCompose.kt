b6d6605b6be16a20c1e92ebc501063c5efc737c6
awkoo <133107732+awkox@users.noreply.github.com>
2026-09-24 11:20:35 +0000

    refactor(app): 搬迁 ComposeTerminalScreen 出 libterminal 到 terminal.shell 包

--- app/src/main/java/com/termux/app/compose/TerminalDetailScreenCompose.kt
+++ app/src/main/java/com/termux/app/compose/TerminalDetailScreenCompose.kt
@@ -84,7 +84,7 @@ import androidx.compose.ui.unit.sp
 import com.termux.R
 import com.awkoo.libterminal.ComposeSessionManager
 import com.awkoo.libterminal.ComposeTerminalSettings
-import com.awkoo.libterminal.ComposeTerminalScreen
+import com.termux.app.terminal.shell.ComposeTerminalScreen
 import com.awkoo.libterminal.engine.TerminalSession as LibTerminalSession
 import com.awkoo.libterminal.engine.pid
 import com.awkoo.libterminal.engine.sessionExited
